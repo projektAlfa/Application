dc6036429dd9ebb56526db9bbdc4cf61006b4c87
Julia <julaszym12@gmail.com>
2019-11-15 13:30:42 +0100

    modified:   JsonApp/JsonApp/JsonApp.Android/JsonApp.Android.csproj 	modified:   JsonApp/JsonApp/JsonApp.Android/JsonApp.Android.csproj.user 	modified:   JsonApp/JsonApp/JsonApp.Android/MainActivity.cs 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/Resource.designer.cs 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-hdpi/icon.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-hdpi/launcher_foreground.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-mdpi/icon.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-mdpi/launcher_foreground.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xhdpi/icon.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xhdpi/launcher_foreground.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xxhdpi/icon.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xxhdpi/launcher_foreground.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xxxhdpi/icon.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/mipmap-xxxhdpi/launcher_foreground.png 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/values/colors.xml 	modified:   JsonApp/JsonApp/JsonApp.Android/Resources/values/styles.xml 	modified:   JsonApp/JsonApp/JsonApp.Android/obj/JsonApp.Android.csproj.nuget.cache 	modified:   JsonApp/JsonApp/JsonApp.Android/obj/JsonApp.Android.csproj.nuget.dgspec.json 	modified:   JsonApp/JsonApp/JsonApp.Android/obj/JsonApp.Android.csproj.nuget.g.props 	modified:   JsonApp/JsonApp/JsonApp.Android/obj/project.assets.json 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/JsonApp.iOS.csproj.nuget.cache 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/JsonApp.iOS.csproj.nuget.dgspec.json 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/JsonApp.iOS.csproj.nuget.g.props 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/iPhoneSimulator/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/iPhoneSimulator/Debug/JsonApp.iOS.csprojAssemblyReference.cache 	modified:   JsonApp/JsonApp/JsonApp.iOS/obj/project.assets.json 	modified:   JsonApp/JsonApp/JsonApp/obj/JsonApp.csproj.nuget.cache 	modified:   JsonApp/JsonApp/JsonApp/obj/JsonApp.csproj.nuget.dgspec.json 	modified:   JsonApp/JsonApp/JsonApp/obj/JsonApp.csproj.nuget.g.props 	modified:   JsonApp/JsonApp/JsonApp/obj/JsonApp.csproj.nuget.g.targets 	modified:   JsonApp/JsonApp/JsonApp/obj/project.assets.json 	JsonApp/.vs/ 	JsonApp/JsonApp/.vs/ 	JsonApp/JsonApp/JsonApp.Android/Resources/drawable/ 	JsonApp/JsonApp/JsonApp.Android/SplashScreen.cs 	JsonApp/JsonApp/JsonApp.Android/obj/Release/ 	JsonApp/JsonApp/JsonApp/obj/Release/

--- JsonApp/JsonApp/JsonApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ JsonApp/JsonApp/JsonApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -63,7 +63,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"8e4d0733-95bf-45d4-a08f-f59a56fc7daa"
+	.asciz	"90c4a83e-9fec-4ca5-af51-c78c2cdfcef3"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
